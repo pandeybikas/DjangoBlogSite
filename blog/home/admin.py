8d4e6135aeca747d2b1676d1dc0af63b0035c79a
pandeybikas <bikaspandey92@gmail.com>
2023-05-14 15:13:14 +0530

    created profile model in home app

--- blog/home/admin.py
+++ blog/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Author, Category, Blog
 # Register your models here.
+class ModelBlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish_date']
+    prepopulated_fields = {'slug' : ('title',)}
+admin.site.register(Blog, ModelBlogAdmin)
+admin.site.register(Category)
+admin.site.register(Author)
--- blog/home/models.py
+++ blog/home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name= models.CharField(max_length=100, null=True)
+    pic = models.ImageField(upload_to='media', null=True)
+    bio = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    item = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.item    
+
+
+class Blog(models.Model):
+    title= models.CharField(max_length=100, null=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+    image= models.ImageField(upload_to='media', null=True)
+    desc = models.TextField(null=True)
+    author= models.ForeignKey(Author, on_delete=models.PROTECT, null=True)
+    category= models.ForeignKey(Category, on_delete=models.PROTECT, null=True)
+    publish_date = models.DateField(auto_now=True, null=True)
+    approved = models.BooleanField(null=True, default=False)
+    def __str__(self):
+        return self.title
