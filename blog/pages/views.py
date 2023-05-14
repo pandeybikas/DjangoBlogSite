88c4b2a737d2ced43be823d240a0cd5399bbf20f
pandeybikas <bikaspandey92@gmail.com>
2023-05-14 23:11:47 +0530

    added pagination function to blogs

--- blog/pages/views.py
+++ blog/pages/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from home.models import Blog
+from django.core.paginator import Paginator
 # Create your views here.
 def health(request):
     health_blogs = Blog.objects.filter(category_id=1)
+    pagination = Paginator(health_blogs, 6)
+    page_number = request.GET.get('page')
+    health_blogs= pagination.get_page(page_number)
     return render(request, 'health.html', {'health_blogs': health_blogs})
 
 
 
 def travel(request):
     travel_blog = Blog.objects.filter(category_id=2)
+    pagination = Paginator(travel_blog, 6)
+    page_number = request.GET.get('page')
+    travel_blog = pagination.get_page(page_number)
     return render(request, 'travel.html', {'travel_blog': travel_blog})
 
 
@@ -16,6 +23,9 @@ def travel(request):
 
 def news(request):
     news_blog = Blog.objects.filter(category_id=3)
+    pagination = Paginator(news_blog, 6)
+    page_number = request.GET.get('page')
+    news_blog= pagination.get_page(page_number)
     return render(request, 'news.html',{'news_blog': news_blog})
 
 
@@ -23,4 +33,7 @@ def news(request):
 
 def movies(request):
     movie_blog = Blog.objects.filter(category_id=4)
+    pagination = Paginator(movie_blog, 6)
+    page_number = request.GET.get('page')
+    movie_blog= pagination.get_page(page_number)
     return render(request, 'movies.html', {'movie_blog': movie_blog})
